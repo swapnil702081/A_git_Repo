5cdd791100701f60f9680aff2d5397fdcd6ea92b
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 14:38:01 +0530

    [SwapnilG]: New Program

---
+++ src/NEW/Swapnil.java
@@ -0,0 +1,18 @@
+package NEW;
+
+public class Swapnil 
+{
+
+	static
+	{
+		System.out.println("My name is \"Swapnil Gole\".");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+}
